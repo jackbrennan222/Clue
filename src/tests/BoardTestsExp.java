ba9901b7eba38cc276b9c51151f8e77dcd60af18
eswanson3 <eswanson1@mines.edu>
2022-10-06 12:12:37 -0600

    added tests in boardtests

--- src/tests/BoardTestsExp.java
+++ src/tests/BoardTestsExp.java
@@ -2,8 +2,29 @@ package tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Test;
 import org.junit.jupiter.*;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.Set;
+
+import experiment.TestBoard;
+import experiment.TestBoardCell;
 
 public class BoardTestsExp {
+	TestBoard board;
 	
+	@BeforeEach
+	public void setup() {
+		board = new TestBoard(4, 4);
+	}
+	
+	@Test
+	public void testAdjacency() {
+		TestBoardCell cell = board.getCell(0, 0);
+		Set<TestBoardCell> testList = cell.getAdjList();
+		assertTrue(testList.contains(board.getCell(1, 0)));
+		assertTrue(testList.contains(board.getCell(0, 1)));
+		assertEquals(2, testList.size());
+	}
 }
