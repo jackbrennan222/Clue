984e545f135b1e03261c22966a9bfdd0ad3f9685
eswanson3 <eswanson1@mines.edu>
2022-10-10 12:33:43 -0600

    made progress on fixing up testtargetsnormal

--- src/tests/BoardTestsExp.java
+++ src/tests/BoardTestsExp.java
@@ -18,7 +18,7 @@ public class BoardTestsExp {
 	}
 	
 	@Test
-	public void testAdjacency1() {
+	public void testAdjacency() {
 		TestBoardCell cell = board.getCell(0, 0); //store the cell at location in a variable
 		Set<TestBoardCell> testList = cell.getAdjList(); //get the adjList of the cell
 		assertTrue(testList.contains(board.getCell(0, 1)));
@@ -65,28 +65,36 @@ public class BoardTestsExp {
 		assertEquals(3, targets.size()); //test if list is the right size
 		
 		TestBoardCell cell1 = board.getCell(0, 0); //store the cell at location in a variable
-		board.calcTargets(cell1, 4); //calc targets for the cell with given pathLength
+		board.calcTargets(cell1, 3); //calc targets for the cell with given pathLength
 		Set<TestBoardCell> targets1 = board.getTargets(); //get the target list of the target calc
-		
-		assertEquals(7, targets1.size()); //test if list is the right size
+		assertTrue(targets1.contains(board.getCell(0, 1)));
+		assertTrue(targets1.contains(board.getCell(0, 3)));
+		assertTrue(targets1.contains(board.getCell(1, 0)));
+		assertTrue(targets1.contains(board.getCell(1, 2)));
+		assertTrue(targets1.contains(board.getCell(2, 1)));
+		assertTrue(targets1.contains(board.getCell(3, 0)));
+		assertEquals(6, targets1.size()); //test if list is the right size
 		
 		TestBoardCell cell2 = board.getCell(0, 0); //store the cell at location in a variable
-		board.calcTargets(cell2, 5); //calc targets for the cell with given pathLength
+		board.calcTargets(cell2, 4); //calc targets for the cell with given pathLength
 		Set<TestBoardCell> targets2 = board.getTargets(); //get the target list of the target calc
+		assertTrue(targets2.contains(board.getCell(0, 2)));
+		assertTrue(targets2.contains(board.getCell(1, 1)));
+		assertTrue(targets2.contains(board.getCell(1, 3)));
+		assertTrue(targets2.contains(board.getCell(2, 0)));
+		assertTrue(targets2.contains(board.getCell(2, 2)));
+		assertTrue(targets2.contains(board.getCell(3, 1)));
+		assertEquals(6, targets2.size()); //test if list is the right size
 		
-		assertEquals(5, targets2.size()); //test if list is the right size
-		
-		TestBoardCell cell3 = board.getCell(3, 3); //store the cell at location in a variable
+		TestBoardCell cell3 = board.getCell(1, 2); //store the cell at location in a variable
 		board.calcTargets(cell3, 5); //calc targets for the cell with given pathLength
 		Set<TestBoardCell> targets3 = board.getTargets(); //get the target list of the target calc
+		assertEquals(8, targets3.size()); //test if list is the right size
 		
-		assertEquals(4, targets3.size()); //test if list is the right size
-		
-		TestBoardCell cell4 = board.getCell(3, 3); //store the cell at location in a variable
+		TestBoardCell cell4 = board.getCell(1, 2); //store the cell at location in a variable
 		board.calcTargets(cell4, 6); //calc targets for the cell with given pathLength
 		Set<TestBoardCell> targets4 = board.getTargets(); //get the target list of the target calc
-		
-		assertEquals(4, targets4.size()); //test if list is the right size
+		assertEquals(7, targets4.size()); //test if list is the right size
 	}
 	
 	@Test
