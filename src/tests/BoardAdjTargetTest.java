79054f2aa2bcf274376e7b9d3e645339bcaee485
jackbrennan222 <jbrennan@mines.edu>
2022-10-13 12:00:15 -0600

    Class progress on BoardAdjTargetTest

--- src/tests/BoardAdjTargetTest.java
+++ src/tests/BoardAdjTargetTest.java
@@ -24,7 +24,17 @@ public class BoardAdjTargetTest {
 	
 	@Test
 	public void testAdjacenciesRoom() {
+		Set<BoardCell> testList = board.getAdjList(3, 12);
+		assertEquals(1, testList.size());
+		assertTrue(testList.contains(board.getCell(7, 12)));
 		
+		testList = board.getAdjList(10, 3);
+		assertEquals(1, testList.size());
+		assertTrue(testList.contains(board.getCell(7, 3)));
+		
+		testList = board.getAdjList(12, 21);
+		assertEquals(1, testList.size());
+		assertTrue(testList.contains(board.getCell(16, 21)));
 	}
 	
 	@Test
@@ -53,12 +63,38 @@ public class BoardAdjTargetTest {
 	
 	@Test
 	public void testAdjacencyWalkways() {
+		Set<BoardCell> testList = board.getAdjList(24, 7);
+		assertEquals(1, testList.size());
+		assertTrue(testList.contains(board.getCell(23, 7)));
 		
+		testList = board.getAdjList(7, 7);
+		assertEquals(4, testList.size());
+		assertTrue(testList.contains(board.getCell(6, 7)));
+		assertTrue(testList.contains(board.getCell(8, 7)));
+		assertTrue(testList.contains(board.getCell(7, 6)));
+		assertTrue(testList.contains(board.getCell(7, 8)));
+		
+		testList = board.getAdjList(17, 2);
+		assertEquals(2, testList.size());
+		assertTrue(testList.contains(board.getCell(12, 1)));
+		assertTrue(testList.contains(board.getCell(12, 3)));
 	}
 	
 	@Test
 	public void testTargetsInCTLM() {
+		board.calcTargets(board.getRoom('C').getCenterCell(), 1);
+		Set<BoardCell> targets = board.getTargets();
+		assertEquals(2, targets.size());
+		assertTrue(targets.contains(board.getCell(3, 7)));
+		assertTrue(targets.contains(board.getCell(21, 19)));
 		
+		board.calcTargets(board.getCell(3, 3), 2);
+		targets = board.getTargets();
+		assertEquals(4, targets.size());
+		assertTrue(targets.contains(board.getCell(2, 7)));
+		assertTrue(targets.contains(board.getCell(4, 7)));
+		assertTrue(targets.contains(board.getCell(3, 8)));
+		assertTrue(targets.contains(board.getCell(21, 17)));
 	}
 	
 	@Test
@@ -84,6 +120,5 @@ public class BoardAdjTargetTest {
 	@Test
 	public void testTargetsOccupied() {
 		
->>>>>>> 0e4e044362d309303ed46baedff5f5dabe9fc352
 	}
 }
