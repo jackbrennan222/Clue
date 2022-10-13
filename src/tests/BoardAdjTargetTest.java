fa8d596324677b6a62f05a3ea7a63ff2b374b4f3
eswanson3 <eswanson1@mines.edu>
2022-10-13 11:35:54 -0600

    finished adj door test

--- src/tests/BoardAdjTargetTest.java
+++ src/tests/BoardAdjTargetTest.java
@@ -1,9 +1,15 @@
 package tests;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Set;
+
 import org.junit.jupiter.api.*;
 
 import clueGame.Board;
+import clueGame.BoardCell;
 import experiment.*;
 
 public class BoardAdjTargetTest {
@@ -14,5 +20,30 @@ public class BoardAdjTargetTest {
 		board = Board.getInstance();
 		board.setConfigFiles("ClueLayout.csv", "ClueSetup.txt");
 		board.initialize();
+	}
+	
+	@Test
+	public void testAdjacencyDoor()
+	{
+		Set<BoardCell> testList = board.getAdjList(3, 7);
+		assertEquals(4, testList.size());
+		assertTrue(testList.contains(board.getCell(2, 7)));
+		assertTrue(testList.contains(board.getCell(3, 8)));
+		assertTrue(testList.contains(board.getCell(4, 7)));
+		assertTrue(testList.contains(board.getCell(3, 3)));
+
+		testList = board.getAdjList(7, 3);
+		assertEquals(4, testList.size());
+		assertTrue(testList.contains(board.getCell(6, 3)));
+		assertTrue(testList.contains(board.getCell(7, 4)));
+		assertTrue(testList.contains(board.getCell(10, 3)));
+		assertTrue(testList.contains(board.getCell(7, 2)));
+		
+		testList = board.getAdjList(21, 17);
+		assertEquals(4, testList.size());
+		assertTrue(testList.contains(board.getCell(20, 17)));
+		assertTrue(testList.contains(board.getCell(21, 19)));
+		assertTrue(testList.contains(board.getCell(22, 17)));
+		assertTrue(testList.contains(board.getCell(21, 16)));
 	}
 }
