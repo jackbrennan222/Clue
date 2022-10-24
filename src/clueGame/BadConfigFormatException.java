7b674ba6e8f48827e6155d984451c0bcd8088a2b
Jack Brennan <jackbrennan222@gmail.com>
2022-10-24 11:01:42 -0600

    Update BadConfigFormatException.java

--- src/clueGame/BadConfigFormatException.java
+++ src/clueGame/BadConfigFormatException.java
@@ -1,7 +1,5 @@
 package clueGame;
 
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
 public class BadConfigFormatException extends Exception {
 	public BadConfigFormatException() {
 		super("Error: Bad Format");
