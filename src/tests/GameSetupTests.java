5368fe802ef85a6fd0138f084e12d4eceab41a91
eswanson224 <eswanson224@gmail.com>
2022-11-04 23:23:13 -0600

    finished adding comments and adding deal method

--- src/tests/GameSetupTests.java
+++ src/tests/GameSetupTests.java
@@ -2,12 +2,10 @@ package tests;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 
-import org.hamcrest.core.IsInstanceOf;
 import org.junit.jupiter.api.*;
 
 import clueGame.*;
@@ -28,18 +26,19 @@ public class GameSetupTests {
     @Test
     void playersTest() {
         ArrayList<Player> players = board.getPlayers();
-        assertEquals(players.size(), 6);
+        assertEquals(players.size(), 6); // make sure that the proper amount of players are initialized
         
         int humans = 0;
         int robots = 0;
         
+        // count the number of human and computer players
         for (Player p : players) {
         	if (p instanceof HumanPlayer) { humans++; }
         	else if (p instanceof ComputerPlayer) { robots++; }
         }
         
-        assertEquals(humans, 1);
-        assertEquals(robots, 5);
+        assertEquals(humans, 1); // make sure right number of humans were initialized
+        assertEquals(robots, 5); // make sure right number of computers were initialized
     }
 
     @Test
@@ -56,30 +55,46 @@ public class GameSetupTests {
             else if (type == CardType.PERSON) { people++; }
         }
         
-        assertEquals(deck.size(), 21);
-        assertEquals(rooms, 9);
-        assertEquals(weapons, 6);
-        assertEquals(people, 6);
+        assertEquals(deck.size(), 21); // make sure there are right number of cards in the deck
+        assertEquals(rooms, 9); // make sure there are the right number of rooms in the deck
+        assertEquals(weapons, 6); // make sure there are the right number of weapons in the deck
+        assertEquals(people, 6); // make sure there are the right number of people in the deck
+    }
+
+    @Test
+    void testPlayerHands() {
+        HashSet<Card> cards = new HashSet<Card>();
+        Player p1 = board.getPlayers().get(0);
+        Player p2 = board.getPlayers().get(1);
+        Player p3 = board.getPlayers().get(2);
+
+        assertEquals(p1.getHand().size(), 3); // make sure each player has the right number of cards in their hand
+        assertEquals(p2.getHand().size(), 3);
+        assertEquals(p3.getHand().size(), 3);
+
+        // make sure that none of the players have duplicate cards
+        for (Card c : p1.getHand()) {
+            assertTrue(!cards.contains(c));
+            cards.add(c);
+        } 
+
+        for (Card c : p2.getHand()) {
+            assertTrue(!cards.contains(c));
+            cards.add(c);
+        } 
+
+        for (Card c : p3.getHand()) {
+            assertTrue(!cards.contains(c));
+            cards.add(c);
+        } 
     }
 
     @Test
     void testSolution() {
+        // make sure that the solution has the right type of card for each part of the solution
         Solution ans = board.getTheAnswer();
         assertTrue(ans.getPerson().getCardType() == CardType.PERSON);
         assertTrue(ans.getRoom().getCardType() == CardType.ROOM);
         assertTrue(ans.getWeapon().getCardType() == CardType.WEAPON);
-    }
-    
-    @Test
-    void testPlayerHands() {
-    	ArrayList<Player> players = board.getPlayers();
-        assertEquals(players.size(), 6);
-        HashSet<Card> cardSet = new HashSet<Card>();
-        for (Player p : players) {
-            for (Card c : p.getHand()) {
-            	assertFalse(cardSet.contains(c));
-            	cardSet.add(c);
-            }
-        }
     }
 }
