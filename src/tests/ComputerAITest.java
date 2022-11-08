524aaba7a258c3f6ea4a748f6dc0beec97084b05
eswanson224 <eswanson224@gmail.com>
2022-11-07 22:49:17 -0700

    added tests

--- src/tests/ComputerAITest.java
+++ src/tests/ComputerAITest.java
@@ -11,5 +11,25 @@ import org.junit.jupiter.api.*;
 import clueGame.*;
 
 public class ComputerAITest {
+    private Board board;
+
+    @BeforeEach
+    void setup() {
+        // get board instance because it's singleton
+		board = Board.getInstance();
+		// set the proper config files
+		board.setConfigFiles("ClueLayout.csv", "ClueSetup.txt");
+		// initialize the board
+		board.initialize();
+    }
+
+    @Test
+    void selectTargets() {
+
+    }
     
+    @Test
+    void createSuggestion() {
+        
+    }
 }
