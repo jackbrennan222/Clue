c0c285304abdf80b387a79125a5d41f076f8a3fb
eswanson224 <eswanson224@gmail.com>
2022-11-07 22:46:17 -0700

    created ai test file

---
+++ src/tests/ComputerAITest.java
@@ -0,0 +1,15 @@
+package tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import org.junit.jupiter.api.*;
+
+import clueGame.*;
+
+public class ComputerAITest {
+    
+}
