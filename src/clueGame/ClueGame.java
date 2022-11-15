7625d751f334ef2a4ba3ba903848f87beac83ee9
Jack Brennan <jackbrennan222@gmail.com>
2022-11-14 19:55:50 -0700

    Comments Added to ClueGame, BoardCell, and Player

--- src/clueGame/ClueGame.java
+++ src/clueGame/ClueGame.java
@@ -15,35 +15,46 @@ public class ClueGame extends JFrame {
 
     public ClueGame() {
         initialize();
-
+        // setting up constants
         numRows = board.getNumRows();
         numCols = board.getNumColumns();
-
         xDim = numCols * CELL_SIZE;
         yDim = numRows * CELL_SIZE;
-        
+        // JFrame specifications
         setSize(xDim + MENU_SIZE, yDim + MENU_SIZE);
         setLayout(new BorderLayout());
-
+        // subcomponents in frame
         gcp = new GameControlPanel();
         php = new PlayerHandPanel();
         add(php, BorderLayout.EAST);
         add(gcp, BorderLayout.SOUTH);
         add(board, BorderLayout.CENTER);
     }
-
+    
+    /**
+     * setting up the game board
+     */
     private static void initialize() {
         board = Board.getInstance();
         board.setConfigFiles("ClueLayout.csv", "ClueSetup.txt");
         board.initialize();
     }
 
+    /**
+     * updating all components
+     */
     private void update() {
         php.updatePanels();
         board.repaint();
     }
 
+    /**
+     * main method to run program
+     * 
+     * @param args
+     */
     public static void main(String[] args) {
+        // setting up and running clue
         ClueGame clue = new ClueGame();
         clue.setVisible(true);
         clue.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
